f9d3b5ddda670dacd71812041728968466df51af
Poul Henriksen <POHE@zealand.dk>
2023-11-20 10:55:46 +0100

    c3

--- RazorEventMaker23Class/Pages/Countries/CountryIndex.cshtml.cs
+++ RazorEventMaker23Class/Pages/Countries/CountryIndex.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorEventMaker23Class.Interfaces;
+using RazorEventMaker23Class.Models;
 
 namespace RazorEventMaker23Class.Pages.Countries
 {
     public class CountryIndexModel : PageModel
     {
+        private ICountryRepository countryRepository;
+
+        public List<Country> Countries { get; set; }
+        public CountryIndexModel(ICountryRepository crepo)
+        {
+            countryRepository = crepo;
+        }
         public void OnGet()
         {
+            Countries = countryRepository.GetAllCountries();
         }
     }
 }
